bc7d7ba961a310647e0e9c56a6c2b96fa72bb29a
Efraim <efra10_efraim@hotmail.com>
2024-05-09 06:27:43 -0300

    adicionado métodos para ocupar quarto, passear e entregar chave

--- sistemaHospedagem/entidades/Hospede.java
+++ sistemaHospedagem/entidades/Hospede.java
@@ -59,4 +59,89 @@ public class Hospede implements Runnable {
             }
         }
     }
+
+    private void ocuparQuarto(Quarto quarto, Random random) throws InterruptedException {
+        this.hospedesSemQuarto -= quarto.getCapacidade();
+        
+        synchronized (hotel.getTravaRecepcao()) {
+            quarto.ocupar();
+            this.chave = new Chave(this, quarto);
+            this.numeroQuarto = quarto.getNumero();
+            
+            if (this.hospedesSemQuarto > 0) {
+                System.out.println(nome + " ocupou o quarto " + quarto.getNumero() + " com um grupo de " + quarto.getCapacidade() + " hóspedes");
+            } else {
+                System.out.println(nome + " ocupou o quarto " + quarto.getNumero() + " com um grupo de " + this.tamanhoGrupo + " hóspedes");
+            }
+        }
+        
+        Thread.sleep(random.nextInt(2000)); // Tempo de permanência aleatório
+
+        // Vai sair??
+        if (random.nextBoolean()) {
+            sairPorUmTempo(random);
+        }
+        
+        Thread.sleep(random.nextInt(2000));
+
+        synchronized (hotel.getTravaRecepcao()) {
+            quarto.desocupar();
+            System.out.println(nome + " desocupou o quarto " + quarto.getNumero());
+        }
+    }   
+    
+    // Passeio
+    private void sairPorUmTempo(Random random) {
+        System.out.println(nome + " saiu do quarto...");
+        int index = this.hotel.getQuartos().indexOf(this.chave.getQuarto());
+        Quarto quarto = this.hotel.getQuartos().get(index);
+        quarto.sair();
+        
+        Recepcionista recepcionista = selecionarRecepcionista();
+        
+        recepcionista.receberChaveDoHospede(this);
+        
+        try {
+            Thread.sleep(random.nextInt(1000));
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        recepcionista.retornarChaveParaHospede(this);
+        
+        System.out.println(nome + " voltou ao quarto");
+        quarto.voltarAoQuarto();
+    }
+    
+    // Seleciona recepcionista aleatória
+    private Recepcionista selecionarRecepcionista() {
+        try {
+            Random random = new Random();
+            int i = random.nextInt(hotel.getRecepcionistas().size());
+            return hotel.getRecepcionistas().get(i);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    // GETTERS e SETTERS
+    public String getNome() {
+        return nome;
+    }
+
+    public int getTamanhoGrupo() {
+        return tamanhoGrupo;
+    }
+    
+    public void setChave(Chave chave) {
+        this.chave = chave;
+    }
+    
+    public Chave getChave() {
+        return chave;
+    }
+    
+    public int getNumeroQuarto() {
+        return numeroQuarto;
+    }
 }
