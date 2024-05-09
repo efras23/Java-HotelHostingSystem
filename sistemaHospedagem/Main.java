670156a342e019408d46d2f23562b4fc74680ed7
Efraim <efra10_efraim@hotmail.com>
2024-05-09 06:34:17 -0300

    Main implementada, criando sistema do hotel com as devidas threads

--- sistemaHospedagem/Main.java
+++ sistemaHospedagem/Main.java
@@ -1,5 +1,25 @@
 package sistemaHospedagem;
 
-public class Main {
+import sistemaHospedagem.entidades.Camareira;
+import sistemaHospedagem.entidades.Hospede;
+import sistemaHospedagem.entidades.Hotel;
+import sistemaHospedagem.entidades.Recepcionista;
 
+public class Main {
+    public static void main(String[] args) {
+        Hotel hotel = new Hotel();
+
+        // Criando as threads
+        for (Camareira camareira : hotel.getCamareiras()) {
+            new Thread(camareira).start();
+        }
+
+        for (Recepcionista recepcionista : hotel.getRecepcionistas()) {
+            new Thread(recepcionista).start();
+        }
+
+        for (Hospede hospede : hotel.getHospedes()) {
+            new Thread(hospede).start();
+        }
+    }
 }
