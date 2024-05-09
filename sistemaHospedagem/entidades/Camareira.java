214edc3d49b291414d881a782e8c5e4d24d327e3
Arthur <arthuralvesds4@gmail.com>
2024-05-08 23:47:25 -0300

    Adicionando a classe Camareira seus atributos, seu metodo run e o bloco de sincronizacao de threads

--- sistemaHospedagem/entidades/Camareira.java
+++ sistemaHospedagem/entidades/Camareira.java
@@ -1,5 +1,25 @@
 package sistemaHospedagem.entidades;
 
 public class Camareira implements Runnable {
+    private final Hotel hotel;
 
+    public Camareira(Hotel hotel) {
+        this.hotel = hotel;
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            synchronized (hotel.getTravaArrumacao()) {
+                synchronized (hotel.getTravaRecepcao()) {
+                    for (Quarto quarto : hotel.getQuartos()) {
+                        if (!quarto.getOcupado() && !quarto.foiLimpo()) {
+                            quarto.limpar();
+                            System.out.println("Camareira limpou o quarto " + quarto.getNumero());
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
