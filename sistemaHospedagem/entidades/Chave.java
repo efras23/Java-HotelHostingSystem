58a87b4bc20ec2b0948101279e3820a716cb13c9
Arthur <arthuralvesds4@gmail.com>
2024-05-08 23:34:57 -0300

    Adicionando a classe Chave seus atributos e o construtor

--- sistemaHospedagem/entidades/Chave.java
+++ sistemaHospedagem/entidades/Chave.java
@@ -1,5 +1,18 @@
 package sistemaHospedagem.entidades;
 
 public class Chave {
+    private Quarto quarto;
+    private Hospede hospede;
+    
+    public Chave(Hospede hospede, Quarto quarto){
+        this.quarto = quarto;
+    }
 
+    public Quarto getQuarto(){
+        return quarto;
+    }
+
+    public Hospede getHospede(){
+        return hospede;
+    }
 }
