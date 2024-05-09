502c589a3bf62e7afa44855370860d5a42450274
CauaNunes <130007594+Cauanls@users.noreply.github.com>
2024-05-09 00:06:48 -0300

    Adicionando código da entidade Hospede.java
    No código foi adicionado as variáveis que serão utilizadas,o construtor Hospede(Hotel hotel, String nome, int tamanhoGrupo), e a função de alocar o hospede no quarto, assim se tiver quartos disponíveis será alocado no quarto mas se todos quartos estiverem ocupados entra em uma lista de espera

--- sistemaHospedagem/entidades/Hospede.java
+++ sistemaHospedagem/entidades/Hospede.java
@@ -1,5 +1,62 @@
 package sistemaHospedagem.entidades;
 
-public class Hospede implements Runnable {
+import java.util.Random;
 
+@SuppressWarnings("unused")
+  
+public class Hospede implements Runnable {
+    private final Hotel hotel;
+    private final String nome;
+    private final int tamanhoGrupo;
+    private int hospedesSemQuarto;
+    private Chave chave;
+    private int numeroQuarto;
+    private int quantidadeTentativas;
+
+    public Hospede(Hotel hotel, String nome, int tamanhoGrupo) {
+        this.hotel = hotel;
+        this.nome = nome;
+        this.tamanhoGrupo = tamanhoGrupo;
+        this.hospedesSemQuarto = tamanhoGrupo;
+        this.quantidadeTentativas = 0;
+    }
+
+    @Override
+    public void run() {
+        Random random = new Random();
+        Quarto quarto = null;
+        try {
+            Thread.sleep(random.nextInt(5000));
+
+            alocarHospedeNoQuarto(random);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private synchronized void alocarHospedeNoQuarto(Random random) throws InterruptedException {
+        Quarto quarto = hotel.getQuartoDisponivel();
+
+        if (quarto != null) {
+            ocuparQuarto(quarto, random);
+
+            if (this.hospedesSemQuarto <= 0) {
+                return;
+            } else {
+                alocarHospedeNoQuarto(random);
+            }
+        } else {
+            this.quantidadeTentativas += 1;
+
+            if (quantidadeTentativas >= 2) {
+                System.out.println("Hóspede " + this.nome + " não conseguiu um quarto e saiu reclamando");
+                this.hotel.getHospedes().remove(this);
+
+                return;
+            } else {
+                hotel.adicionarHospedeNaListaEspera(this);
+                System.out.println(nome + " adicionado à lista de espera do hotel");
+            }
+        }
+    }
 }
